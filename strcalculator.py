6c398919630353efdc0de7972b14969ddd6801c2
Hlobisile Mlebuka <mlebukah@gmail.com>
2019-06-05 12:50:48 +0200

    refactored string calculator commit

--- strcalculator.py
+++ strcalculator.py
@@ -17,32 +17,35 @@ def add(numberstr):
     elif re.match(r"(\/\/.+\n)", numberstr):            #(//;67\n) and (///***\n)
         delimeter = re.findall(r"((?<=\/\/).*?(?=\n))", numberstr)
         y = delimeter[0]
-        delimeterstring = re.findall(r"((?<=\n)\d+(?=%s)|(?<=%s)\d+(?=%s)|(?<=%s)\d+)" %(y,y,y,y), numberstr)
+        delimeterstring = re.findall(r"((?<=\n)[-+]?\d+(?=%s)|(?<=%s)[-+]?\d+(?=%s)|(?<=%s)[-+]?\d+)" %(y,y,y,y), numberstr)
+        negativenumbers1 = re.findall(r"-\d+", str(delimeterstring))
         delimeterSumTotal = 0
         for delimeternum in delimeterstring:
-            if int(delimeternum) < 1000:
+            if int(delimeternum) < 0:
+                raise Exception("negatives: " + negativenumbers1 + " not allowed")
+            elif int(delimeternum) < 1000:        
                 delimeterSumTotal = delimeterSumTotal + int(delimeternum)
         return delimeterSumTotal
 
     
     elif re.match(r"(^(\d+|-\d+))", numberstr):         #newline characters/no big numbers/exception for negatives
         newlinestring = re.findall(r"(-\d+|\d+)", numberstr)
-        negativenumbers = re.findall(r"(-\d+)", numberstr)
+        negativenumbers = re.findall(r"-\d+", numberstr)
         newlineSumTotal = 0
         for newlinenum in newlinestring:
             if int(newlinenum) < 0:
-                raise ValueError("negatives: " + str(negativenumbers) + " not allowed")
-            else:
-                newlineSumTotal = 0
-                for newlinenum in newlinestring:
-                    if int(newlinenum) < 1000:           
-                        newlineSumTotal = newlineSumTotal + int(newlinenum)
-                return newlineSumTotal
+                raise Exception("negatives: " + str(negativenumbers) + " not allowed")
+            elif int(newlinenum) < 1000:
+                newlineSumTotal = newlineSumTotal + int(newlinenum)
+        return newlineSumTotal
+                
+                
+                    
 
 
-# if __name__ == "__main__":
-#     x = '21,12\n,4,-7,9'
-#     print(add(x))
+if __name__ == "__main__":
+    x = '21,12\n,4,-1,9'
+    print(add(x))
     
 
 
--- test_strcalculator.py
+++ test_strcalculator.py
@@ -3,17 +3,21 @@ import pytest
 
 def test_EmptyStr():
     assert strcalculator.add("") == 0
-    assert strcalculator.add("1,2,3") == (1 + 2 + 3)
-
+    
 def test_newline():    
     assert strcalculator.add("52, 1\n5678, 1, 1") == (52 + 1 + 1 + 1)
 
-def test_delimeters():    
-    assert strcalculator.add("//45;\n245;145;245;2") == (2 + 1 + 2 + 2)
-
+def test_delimeters(): 
+        # import pdb
+        # pdb.set_trace()
+        with pytest.raises(Exception):     
+                assert strcalculator.add("//45;\n245;-145;245;2000") == (2 + 1 + 2 )
+                assert strcalculator.add("//45;\n245;145;245;2000") == (2+1+2)
 def testNegativeNumbers():
-     with pytest.raises(ValueError):
-         assert strcalculator.add("21,12\n,4,-7,9")
+        # import pdb
+        # pdb.set_trace()
+        with pytest.raises(Exception):
+                assert strcalculator.add("21,12\n,4,-7,9")
 
 def testMassiveNumbers():
         assert strcalculator.add("//;\n1;2\n;5;1200") == (1 + 2 + 5)
