e550c39d61689fabb0c4f9a8afbb83d4c44eea22
Hlobisile Mlebuka <mlebukah@gmail.com>
2019-06-05 09:44:55 +0200

    first commit.

--- strcalculator.py
+++ strcalculator.py
@@ -1,22 +1,57 @@
 import re
 
 def add(numberstr):
-    if numberstr == "":
+    if numberstr == "":                                 #empty string
         return 0
-      
-    elif re.findall(r"-\d+", numberstr):
-        negativenumbers = re.findall(r"-\d+", numberstr)
-        raise Exception("negatives: " + str(negativenumbers) + " not allowed")
-        
-    else:
-        SumTotal = 0
-        splitstring = re.findall(r"\d+", numberstr)
-        for num in splitstring:
-            SumTotal += int(num)
-        return SumTotal
-                
+
+
+    elif re.search(r"(\[.\])", numberstr):           #(//[symbols]\n)#multipledelimeter
+        symbolstring = re.findall(r"\d+", numberstr)
+        symbolSumTotal = 0
+        for symbolnum in symbolstring:
+            symbolSumTotal = symbolSumTotal + int(symbolnum)
+        return symbolSumTotal
+
+
+
+    elif re.match(r"(\/\/.+\n)", numberstr):            #(//;67\n) and (///***\n)
+        delimeter = re.findall(r"((?<=\/\/).*?(?=\n))", numberstr)
+        y = delimeter[0]
+        delimeterstring = re.findall(r"((?<=\n)\d+(?=%s)|(?<=%s)\d+(?=%s)|(?<=%s)\d+)" %(y,y,y,y), numberstr)
+        delimeterSumTotal = 0
+        for delimeternum in delimeterstring:
+            if int(delimeternum) < 1000:
+                delimeterSumTotal = delimeterSumTotal + int(delimeternum)
+        return delimeterSumTotal
+
+    
+    elif re.match(r"(^(\d+|-\d+))", numberstr):         #newline characters/no big numbers/exception for negatives
+        newlinestring = re.findall(r"(-\d+|\d+)", numberstr)
+        negativenumbers = re.findall(r"(-\d+)", numberstr)
+        newlineSumTotal = 0
+        for newlinenum in newlinestring:
+            if int(newlinenum) < 0:
+                raise ValueError("negatives: " + str(negativenumbers) + " not allowed")
+            else:
+                newlineSumTotal = 0
+                for newlinenum in newlinestring:
+                    if int(newlinenum) < 1000:           
+                        newlineSumTotal = newlineSumTotal + int(newlinenum)
+                return newlineSumTotal
+
+
+# if __name__ == "__main__":
+#     x = '21,12\n,4,-7,9'
+#     print(add(x))
+    
 
 
 
 
 
+            
+         
+
+       
+            
+    
--- test_strcalculator.py
+++ test_strcalculator.py
@@ -6,14 +6,21 @@ def test_EmptyStr():
     assert strcalculator.add("1,2,3") == (1 + 2 + 3)
 
 def test_newline():    
-    assert strcalculator.add("52, 1\n5678, 1, 1") == (52 + 1 + 5678 + 1 + 1)
+    assert strcalculator.add("52, 1\n5678, 1, 1") == (52 + 1 + 1 + 1)
 
 def test_delimeters():    
-    assert strcalculator.add("//;\n2\n1;2;45;2") == (2 + 1 + 2 + 45 + 2)
+    assert strcalculator.add("//45;\n245;145;245;2") == (2 + 1 + 2 + 2)
 
 def testNegativeNumbers():
-    with pytest.raises(Exception):
-        assert strcalculator.add("//:\n-21:12\n4:-7:9")
+     with pytest.raises(ValueError):
+         assert strcalculator.add("21,12\n,4,-7,9")
 
+def testMassiveNumbers():
+        assert strcalculator.add("//;\n1;2\n;5;1200") == (1 + 2 + 5)
 
+def testAnyLengthDelimeter():
+        assert strcalculator.add("//&&&\n1&&&5&&&6") == (1 + 5 + 6)
+
+def testMultipleDelimeter():
+        assert strcalculator.add("//[&][*][$]\n1*56$1&") == (1 + 56 + 1)
 
