f03ba1e78e9f6e8bcb4e7cd4e67a68b976144830
Hlobisile Mlebuka <mlebukah@gmail.com>
2019-06-12 05:14:05 +0200

    .

--- strcalculator.py
+++ strcalculator.py
@@ -1,60 +1,53 @@
 import re
 
-def add(numberstr):
-    if numberstr == "":                                 #empty string
-        return 0
+def add(numberstring):
 
+    """ This function inputs a sequence of numbers written 
+    in a string format. It then returns the sum of all the numbers
+    within the sequence. It does not include numbers 
+    that are greater than or equal to 1000. It throws an exception for any sequence
+    that contains a negative number. The string calclulator 
+    handle strings of the following format:
+    1) returns an empty string as the number 0.
+    2) multiple delimeters in the form "//[delimeter]\n..." or "//[&][^]...\n..."
+    3) sequences with new line characters """
 
-    elif re.search(r"(\[.\])", numberstr):           #(//[symbols]\n)#multipledelimeter
-        symbolstring = re.findall(r"\d+", numberstr)
-        symbolSumTotal = 0
-        for symbolnum in symbolstring:
-            symbolSumTotal = symbolSumTotal + int(symbolnum)
-        return symbolSumTotal
+    extracted_string = extracting_numbers_from_the_string(numberstring)
+    sum_of_string_numbers = addition_of_extracted_strings(extracted_string)
+     
+    return sum_of_string_numbers
 
-
-
-    elif re.match(r"(\/\/.+\n)", numberstr):            #(//;67\n) and (///***\n)
-        delimeter = re.findall(r"((?<=\/\/).*?(?=\n))", numberstr)
-        y = delimeter[0]
-        delimeterstring = re.findall(r"((?<=\n)[-+]?\d+(?=%s)|(?<=%s)[-+]?\d+(?=%s)|(?<=%s)[-+]?\d+)" %(y,y,y,y), numberstr)
-        negativenumbers1 = re.findall(r"-\d+", str(delimeterstring))
-        delimeterSumTotal = 0
-        for delimeternum in delimeterstring:
-            if int(delimeternum) < 0:
-                raise Exception("negatives: " + negativenumbers1 + " not allowed")
-            elif int(delimeternum) < 1000:        
-                delimeterSumTotal = delimeterSumTotal + int(delimeternum)
-        return delimeterSumTotal
-
+def extracting_numbers_from_the_string(numberstring):
+    if numberstring == "":                                  #empty strings return a zero
+        extracted_string = '0'
+        
+    elif re.search(r"(\[.\])", numberstring):               #matches '//[&][*][$]\n...&'         
+        extracted_string = re.findall(r"\d+", numberstring)
     
-    elif re.match(r"(^(\d+|-\d+))", numberstr):         #newline characters/no big numbers/exception for negatives
-        newlinestring = re.findall(r"(-\d+|\d+)", numberstr)
-        negativenumbers = re.findall(r"-\d+", numberstr)
-        newlineSumTotal = 0
-        for newlinenum in newlinestring:
-            if int(newlinenum) < 0:
-                raise Exception("negatives: " + str(negativenumbers) + " not allowed")
-            elif int(newlinenum) < 1000:
-                newlineSumTotal = newlineSumTotal + int(newlinenum)
-        return newlineSumTotal
-                
-                
-                    
-
+    elif re.match(r"(\/\/.+\n)", numberstring):             #matches "//[delimeter]\n245;145;245;2000"      
+        delimeter_string = re.findall(r"((?<=\/\/).*?(?=\n))", numberstring)
+        y = delimeter_string[0]                             #convert delimeter_string into a string that can be handled by the extracted_string regex
+        extracted_string = re.findall(r"((?<=\n)[-+]?\d+(?=%s)|(?<=%s)[-+]?\d+(?=%s)|(?<=%s)[-+]?\d+)" %(y,y,y,y), numberstring)  #this regular expression also uses
+        
+    elif re.match(r"(^(\d+|-\d+))", numberstring):          #matches '-52, 1\n5, 1, 1'
+        extracted_string = re.findall(r"(-\d+|\d+)", numberstring)
+        
+    return extracted_string
+        
+def addition_of_extracted_strings(extracted_string):        #handles string of numbers extracted from different formats
+    negativenumbers = re.findall(r"-\d+", str(extracted_string))
+    sum_of_string_numbers = 0
+    for number in extracted_string:
+        
+        if int(number) < 0:                                 #no negative numbers allowed
+            raise Exception("Negatives: " + str(negativenumbers) + " Not Allowed" )
+        
+        elif int(number) < 1000:                            #numbers larger than 1000 ignored
+            sum_of_string_numbers = sum_of_string_numbers + int(number)
+    
+    return sum_of_string_numbers
 
 if __name__ == "__main__":
-    x = '21,12\n,4,-1,9'
-    print(add(x))
-    
-
-
-
-
-
-            
-         
-
-       
-            
-    
+     
+    numberstring = '//[&][*][$]\n1*56$1&'
+    print(add(numberstring))  
--- test_strcalculator.py
+++ test_strcalculator.py
@@ -4,23 +4,22 @@ import pytest
 def test_EmptyStr():
     assert strcalculator.add("") == 0
     
-def test_newline():    
-    assert strcalculator.add("52, 1\n5678, 1, 1") == (52 + 1 + 1 + 1)
-
+def test_newline():
+        assert strcalculator.add("52, 1\n5, 1, 1") == (52 + 1 + 1 + 5 + 1)
+                 
 def test_delimeters(): 
-        # import pdb
-        # pdb.set_trace()
-        with pytest.raises(Exception):     
-                assert strcalculator.add("//45;\n245;-145;245;2000") == (2 + 1 + 2 )
-                assert strcalculator.add("//45;\n245;145;245;2000") == (2+1+2)
-def testNegativeNumbers():
-        # import pdb
-        # pdb.set_trace()
-        with pytest.raises(Exception):
-                assert strcalculator.add("21,12\n,4,-7,9")
+        assert strcalculator.add("//45;\n245;145;245;2000") == (2+1+2)
 
+def testNegativeNumbers():
+        with pytest.raises(Exception):
+                assert strcalculator.add("21,12\n4,-7,9") == (21 + 12 + 4 + 9 -7)
+                assert strcalculator.add("//45;\n245;-145;245;2000") == (2 + 1 + 2 + -1 + 2000)
+                assert strcalculator.add("//[&][*][$]\n1*56$-1&") == (1 + 56 - 1)
 def testMassiveNumbers():
         assert strcalculator.add("//;\n1;2\n;5;1200") == (1 + 2 + 5)
+        assert strcalculator.add("//45;\n245;145;245;2000") == (2+1+2)
+        assert strcalculator.add("//[&][*][$]\n1*56$1&2000") == (1 + 56 + 1)
+        assert strcalculator.add("//&&&\n1&&&5&&&6&&&3000") == (1 + 5 + 6)
 
 def testAnyLengthDelimeter():
         assert strcalculator.add("//&&&\n1&&&5&&&6") == (1 + 5 + 6)
